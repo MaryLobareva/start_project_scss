c818f511099c6714d6572271c912d7ca02b33981
Mary Lobareva <marylobareva@gmail.com>
2018-02-28 12:26:17 +0300

    start

---
+++ config.rb
@@ -0,0 +1,19 @@
+
+http_path = "/"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "img"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+
+# output_style = :compressed 
+output_style = :expanded 
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+ line_comments = true
+
+
